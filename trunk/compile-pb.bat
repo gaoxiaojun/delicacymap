f1460589ccd2cad351251f3380a425a14e6fcb0d
colprog <colprog@d4fe8cd8-f54c-11dd-8c4e-4bbb75a408b7>
2009-10-18 08:48:50 +0000

    clean up some legacy file.

--- trunk/compile-pb.bat
+++ trunk/compile-pb.bat
@@ -1,3 +1,2 @@
 set DST_DIR=protocol-buffer-src
 protoc --cpp_out=%DST_DIR% MapProtocol.proto
-protoc --cpp_out=%DST_DIR% Message.proto
--- trunk/deliciousClient/GenVCProj_Wince.bat
+++ trunk/deliciousClient/GenVCProj_Wince.bat
@@ -1,1 +1,1 @@
-F:\QT\qt-4.6.0-tp1\bin\qmake -t vcapp
+F:\QT\qt-4.6.0-beta1\bin\qmake -t vcapp
