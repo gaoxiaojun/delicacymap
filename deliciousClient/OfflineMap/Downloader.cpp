4f550ae3726400b135a4c965fa823c993c2ce5f8
colprog <colprog@d4fe8cd8-f54c-11dd-8c4e-4bbb75a408b7>
2010-02-15 23:53:16 +0000

    switch to sqlite to store. commit before merging to main branch

--- deliciousClient/OfflineMap/Downloader.cpp
+++ deliciousClient/OfflineMap/Downloader.cpp
@@ -1,17 +1,15 @@
 #include "Downloader.h"
 #include <QUrl>
-#include <QDir>
-#include <QFileInfo>
-#include <QDebug>
 #include <QNetworkRequest>
 #include <QNetworkReply>
+#include <QSqlQuery>
 
 bool operator== (const Downloader::Request& arg1, const Downloader::Request& arg2){
-	return (arg1.url == arg2.url) && (arg1.filename == arg2.filename);
+	return arg1.tile == arg2.tile;
 }
 
 Downloader::Downloader()
-:aborted(false), lastRequestId(0), network(this){
+:network(this){
     connect(&network, SIGNAL(finished(QNetworkReply*)), this, SLOT(requestFinished(QNetworkReply*)));
 }
 
@@ -28,31 +26,26 @@ void Downloader::setProxy(const QString& host, int port, const QString& userName
 }
 
 void Downloader::requestFinished(QNetworkReply * reply){
-    QString& filename = replytofile[reply];
+    TileCoord tile = replytofile[reply];
     if (reply->isFinished())
     {
         QVariant &header = reply->header(QNetworkRequest::ContentTypeHeader);
         if (header.isValid() && header.toString().contains("image", Qt::CaseInsensitive))
         {
-            QFile output(filename);
-            QFileInfo fileinfo(filename);
-            QDir dir;
-            dir.mkpath(fileinfo.absolutePath());
-            if (!output.open(QIODevice::WriteOnly))
-            {
-     			qDebug()<<"couldn't open file "<<filename;
-            }
-            output.write(reply->readAll());
+            QSqlQuery query;
+            query.prepare(QString("INSERT INTO [%1] VALUES(:x, :y, :data)").arg(tile.zoom));
+            query.bindValue(0, tile.x);
+            query.bindValue(1, tile.y);
+            query.bindValue(2, reply->readAll());
+            query.exec();
         }
     }
     replytofile.remove(reply);
     reply->deleteLater();
 }
 
-void Downloader::addDownload(const QString& url, const QString& filename){
-	qDebug()<<"addDownload("<<url<<", "<<filename;
-	aborted = false;
-	Request request(url, filename);
+void Downloader::addDownload(const QString& url, int x, int y, int zoom){
+	Request request(url, x, y, zoom);
 	if (!inProcessing(request)){
 		requests.enqueue(request);
 		if (requests.count() == 1){
@@ -70,28 +63,21 @@ bool Downloader::inProcessing(const Downloader::Request& request){
 	return false;
 }
 
-Downloader::Request::Request(const QString& _url, const QString& _filename)
-:url(_url), filename(_filename){
+Downloader::Request::Request(const QString& _url, int x, int y, int zoom)
+:url(_url)
+{
+    tile.x = x;
+    tile.y = y;
+    tile.zoom = zoom;
 }
 
 void Downloader::nextRequest(){
-	qDebug()<<"next request\n";
 	while (!requests.empty()){
 		Request& cur = requests.head();
-//		file.setFileName(cur.filename);
         QNetworkRequest request(cur.url);
 		QNetworkReply* reply = network.get(request);
-        replytofile.insert(reply, cur.filename);
+        replytofile.insert(reply, cur.tile);
         requests.dequeue();
-// 		if (!file.open(QIODevice::WriteOnly)){
-// 			qDebug<<"couldn't open file "<<cur.filename;
-// 			//cur.callback->execute(false);
-// 			requests.dequeue();
-// 			continue;
-// 		}
-		
-// 		printf("file to retrieve: %s\n", (const char*)(cur.url.toAscii()));
-// 		lastRequestId = http->get(cur.url, &file);
 		break;
 	}
 	
@@ -99,18 +85,21 @@ void Downloader::nextRequest(){
 		emit downloadsFinished();
 }
 
-bool Downloader::isLoadingUrl(const QString& url){
+bool Downloader::isLoadingUrl(const QString& url) const
+{
 	for (int i = 0; i < requests.count(); i++){
-		if (requests[i].url == url) return 
-			true;
+		if (requests[i].url == url) 
+            return true;
 	}
 	return false;
 }
 
-bool Downloader::isLoadingFile(const QString& filename){
-	for (int i = 0; i < requests.count(); i++){
-		if (requests[i].filename == filename) return 
-			true;
-	}
-	return false;
+bool Downloader::isLoadingTile( int x, int y, int zoom ) const
+{
+    TileCoord requesttile = {x, y, zoom};
+    for (int i = 0; i < requests.count(); i++){
+        if (requests[i].tile == requesttile) 
+            return true;
+    }
+    return false;
 }
--- deliciousClient/OfflineMap/Downloader.h
+++ deliciousClient/OfflineMap/Downloader.h
@@ -1,59 +1,39 @@
-/*
-QGMView - Qt Google Map Viewer
-Copyright (C) 2007  Victor Eremin
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-You can contact author using following e-mail addreses
-erv255@googlemail.com 
-ErV2005@rambler.ru
-*/
 #ifndef DOWNLOADER_H
 #define DOWNLOADER_H
 #include <QObject>
 #include <QNetworkAccessManager>
+#include <QSqlDatabase>
 #include <QQueue>
-#include <QFile>
 #include <QHash>
 
 class Downloader: public QObject{
 Q_OBJECT
 public:
+    struct TileCoord
+    {
+        bool operator==(const TileCoord& other) const{ return x == other.x && y == other.y && zoom == other.zoom; }
+        int x, y, zoom;
+    };
 	class Request{
 	public:
 		QString url;
-		QString filename;
-		Request(const QString& url, const QString& filename);
+		TileCoord tile;
+		Request(const QString& url, int x, int y, int zoom);
 	};
 
-	void addDownload(const QString& url, const QString& fileName);
+	void addDownload(const QString& url, int x, int y, int zoom);
 	void setProxy(const QString& host, int port, const QString& userName = QString(), const QString& password = QString());
+    void setDB(const QSqlDatabase& db) { this->db = db; }
 	Downloader();
-	bool isLoadingUrl(const QString& url);
-	bool isLoadingFile(const QString& filename);
+	bool isLoadingUrl(const QString& url) const;
+    bool isLoadingTile(int x, int y, int zoom) const;
 signals:
 	void downloadsFinished();
 	void downloadStarted();
 protected:
-	enum {MaxDownloads = 8};
-
-	QFile file;
-	bool aborted;
-	int lastRequestId;
+	QSqlDatabase db;
 	QQueue<Request> requests;
-    QHash<QNetworkReply*, QString> replytofile;
+    QHash<QNetworkReply*, TileCoord> replytofile;
 	QNetworkAccessManager network;
 	void nextRequest();
 	bool inProcessing(const Request& request);
